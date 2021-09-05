814849dcc2cdcf5bfe3f4306c9ab0bed5806d821
Alejandro Gonzalez <myname123210@gmail.com>
2021-09-05 10:24:21 -0500

    Initial Commit

---
+++ shoppingList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shoppingList
+//
+//  Created by Alejandro Gonzalez on 9/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
